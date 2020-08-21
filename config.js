236b6233b32530fe66fc94fd8415fa60c7978214
Juan David Sarmiento <juandsarmiento@gmail.com>
2020-08-21 10:58:35 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    DB_URL: process.env.DB_URL || 'mongodb+srv://db_user_taller_node:DLFjcVUsPyLQOsHF@cluster0.11ztz.azure.mongodb.net/db_taller_node?retryWrites=true&w=majority',
-    PORT: process.env.PORT || 3000,
-    HOST: process.env.HOST || 'http://localhost',
-    PUBLIC_PATH: process.env.PUBLIC_PATH || '/app'
-}
-
-module.exports = config;
